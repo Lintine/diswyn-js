888e9c8137a82a0600904630dc67640bc7d1ca99
Mizu <40831463+Rexxt@users.noreply.github.com>
2021-12-10 20:35:05 +0100

    fuck node

--- index.js
+++ index.js
@@ -18,3 +18,45 @@
 // along with Diswyn.  If not, see <https://www.gnu.org/licenses/>.
 //
 //------------------------------------------------------------------------------
+const Discord = require('discord.js');
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+
+const { DISCORD_TOKEN, ADDITIONAL_OWN_PERMS } = require('./env.json');
+const { status } = require('./data.json');
+
+const client = new Discord.Client();
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+client.once('ready', () => {
+	console.log("Logged in as");
+    console.log(`| Username: ${client.user.username}`);
+    console.log(`| ID: ${client.user.id}`);
+    console.log(`| Status: ${status}`);
+    console.log("-------------------------------");
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+// Connecting to the Discord API
+client.login(token);

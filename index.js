822549325f9055cee7bad0caa3ea4d8f984ea251
Mizu <40831463+Rexxt@users.noreply.github.com>
2021-12-10 19:52:34 +0100

    Proper initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Diswyn, a general-purpose, modular and self-hostable Discord bot
+// Copyleft: 2021, Lintine
+// License: GPLv3
+//
+// This file is part of Diswyn.
+//
+// Diswyn is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Diswyn is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Diswyn.  If not, see <https://www.gnu.org/licenses/>.
+//
+//------------------------------------------------------------------------------
